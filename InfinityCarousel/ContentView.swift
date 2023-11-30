3dcf9055e8d650824b8d50cf05728e7e384949b2
profit0124 <profit0124@gmail.com>
2023-11-30 23:19:25 +0900

    Initial Commit

---
+++ InfinityCarousel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InfinityCarousel
+//
+//  Created by Sooik Kim on 11/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfinityCarousel/InfinityCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityCarouselApp.swift
+//  InfinityCarousel
+//
+//  Created by Sooik Kim on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct InfinityCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
