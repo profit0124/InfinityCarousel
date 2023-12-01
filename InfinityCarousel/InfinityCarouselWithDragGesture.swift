50cc75ac5189341f7d865a193c80943115f8c269
profit0124 <profit0124@gmail.com>
2023-12-01 22:43:43 +0900

    first commit

---
+++ InfinityCarousel/InfinityCarouselWithDragGesture.swift
@@ -0,0 +1,18 @@
+//
+//  InfinityCarouselWithDragGesture.swift
+//  InfinityCarousel
+//
+//  Created by Sooik Kim on 12/1/23.
+//
+
+import SwiftUI
+
+struct InfinityCarouselWithDragGesture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfinityCarouselWithDragGesture()
+}
